c1bd64c56c1152f0858a3c26fdb776d2f22dc9c8
Oleg Lozovik <oldisc3@gmail.com>
2021-06-01 20:47:29 +0300

    task-03

---
+++ js/task-03.js
@@ -0,0 +1,40 @@
+/* Напиши скрипт для создания галлереи изображений по массиву данных.
+В HTML есть список ul#gallery.
+Используй массив объектов images для создания тегов img вложенных в li.
+Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через css-классы. */
+
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const createGallery = function (arr) {
+  const arrEls = arr.map(item => {
+    return `<li><img src="${item.url}" alt="${item.alt}"></li>`;
+  });
+  return arrEls.join('');
+};
+
+const galleryEl = document.querySelector('#gallery');
+galleryEl.insertAdjacentHTML('afterbegin', createGallery(images));
+
+galleryEl.classList.add('gallery');
+
+const itemsEls = galleryEl.querySelectorAll('li');
+itemsEls.forEach(item => {
+  item.classList.add('gallery__item');
+  item.firstChild.classList.add('gallery__img');
+});
